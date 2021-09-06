298c544c263e9825e7dc521fb345f583112abc8a
umitturk <uyt_turk@hotmail.com>
2021-09-06 16:39:05 +0300

    yeniredux

---
+++ movie-app/movieapp/app/models/movie.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export default function movie({id,title,poster_path,backdrop_path,genres,overview,popularity,vote_avarage,vote_count, genre_ids}) {
+    return (
+        <View>
+            {
+                id
+            }
+        </View>
+    )
+}
